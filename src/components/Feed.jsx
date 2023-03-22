8d060916e69c4abfd39f0785b2449eb552b71ad1
ko-shiro-hap <koushirou.iino@icloud.com>
2023-03-22 22:29:32 +0900

    Sidebarの見た目を作成

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import Sidebar from './Sidebar'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 3 } }}>
+        <Sidebar />
+        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright 2023 Koshiro Iino
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
