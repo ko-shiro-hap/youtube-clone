80465bc8a09498a8b57745c36a218788c842a72c
ko-shiro-hap <koushirou.iino@icloud.com>
2023-03-23 13:08:49 +0900

    ChannelDetailを作成

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -32,7 +32,7 @@ const Feed = () => {
         <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white' }}>
           {selectedCategory} <span style={{ color: '#F31503' }}>videos</span>
         </Typography>
-        <Videos videos={[videos]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,7 +6,7 @@ const Videos = ({ videos }) => {
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start"
     gap={2}>
-      {[...videos][0].map((item, idx) => {
+      {videos.map((item, idx) => {
         return (
           <Box key={idx}>
             {item.id.videoId && <VideoCard video={item} /> }
